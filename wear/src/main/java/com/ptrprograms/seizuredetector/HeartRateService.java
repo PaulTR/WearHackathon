a0a160d09e21136d1bdad49c60e1162a3ada79b9
Paul Ruiz <paultr@sportslabs.com>
2014-11-01 15:03:06 -0600

    reverting back to a timer for heart rate sensor since the normal sensor takes ~20-30 seconds to return a result

--- wear/src/main/java/com/ptrprograms/seizuredetector/HeartRateService.java
+++ wear/src/main/java/com/ptrprograms/seizuredetector/HeartRateService.java
@@ -5,14 +5,12 @@ import android.content.Intent;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by paulruiz on 11/1/14.
  */
 public class HeartRateService extends Service implements HeartRateDetector.Listener {
 
-    private int mHeartrate = -1;
     private HeartRateDetector hrd;
 
     @Override
@@ -22,22 +20,25 @@ public class HeartRateService extends Service implements HeartRateDetector.Liste
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        //SensorManager sensorManager = (SensorManager) getSystemService( SENSOR_SERVICE );
-        //HeartRateDetector hrd = new HeartRateDetector( this );
-        //hrd.start( sensorManager );
-
+        /*
+        SensorManager sensorManager = (SensorManager) getSystemService( SENSOR_SERVICE );
+        if( hrd == null )
+            hrd = new HeartRateDetector( this );
+        hrd.start( sensorManager );
+        */
         Log.e( "Heart Rate", "onStartCommand" );
+
         new Thread( new Runnable() {
             @Override
             public void run() {
                 try {
                     Thread.sleep( 3000 );
                 } catch( InterruptedException e ) {
-                    Log.e("HeartRateService", "Shit broke, wat: " + e.getMessage() );
                 }
                 heartRate( 85 );
             }
         }).run();
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -46,12 +47,9 @@ public class HeartRateService extends Service implements HeartRateDetector.Liste
     public void heartRate(int heartRate) {
 
         Log.e( "HeartRateService", "heartrate: " + heartRate );
-        if( mHeartrate == -1 ) {
-            mHeartrate = heartRate;
-            Intent intent = new Intent( getApplicationContext(), TimerService.class );
-            intent.setAction( TimerService.ACTION_SHOW_ALARM );
-            startService( intent );
-        }
+        Intent intent = new Intent( getApplicationContext(), TimerService.class );
+        intent.setAction( TimerService.ACTION_SHOW_ALARM );
+        startService( intent );
 
         if( hrd != null ) {
             hrd.stop();
